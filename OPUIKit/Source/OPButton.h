c5bb50ed5df3168da64461725e27fbadda5cc515
Brandon Williams <brandon@fewbutripe.com>
2012-01-14 17:21:15 -0500

    A subclass of UIButton for delegating drawing to blocks.

---
+++ OPUIKit/Source/OPButton.h
@@ -0,0 +1,18 @@
+//
+//  OPButton.h
+//  OPUIKit
+//
+//  Created by Brandon Williams on 1/14/12.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OPButton;
+typedef void(^OPButtonDrawingBlock)(OPButton* b, CGRect r, CGContextRef c);
+
+@interface OPButton : UIButton
+
+-(void) addDrawingBlock:(OPButtonDrawingBlock)block forState:(UIControlState)state;
+
+@end
---
+++ OPUIKit/Source/OPButton.m
@@ -0,0 +1,44 @@
+//
+//  OPButton.m
+//  OPUIKit
+//
+//  Created by Brandon Williams on 1/14/12.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
+//
+
+#import "OPButton.h"
+#import "OPMacros.h"
+
+@interface OPButton (/**/)
+@property (nonatomic, strong) NSMutableDictionary *drawingBlocks;
+@end
+
+@implementation OPButton
+
+@synthesize drawingBlocks;
+
+-(void) addDrawingBlock:(OPButtonDrawingBlock)block forState:(UIControlState)state {
+    
+    if (! self.drawingBlocks)
+        self.drawingBlocks = [NSMutableDictionary new];
+    if (! [self.drawingBlocks objectForKey:NSInt(state)])
+        [self.drawingBlocks setObject:[NSMutableArray new] forKey:NSInt(state)];
+    
+    [[self.drawingBlocks objectForKey:NSInt(state)] addObject:[block copy]];
+}
+
+-(void) drawRect:(CGRect)rect {
+    [super drawRect:rect];
+    
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    for (NSNumber *drawState in self.drawingBlocks)
+    {
+        if (([drawState intValue] & self.state) || (self.state == UIControlStateNormal && [drawState intValue] == UIControlStateNormal))
+        {
+            for (OPButtonDrawingBlock block in [self.drawingBlocks objectForKey:drawState])
+                block(self, rect, c);
+        }
+    }
+}
+
+@end
