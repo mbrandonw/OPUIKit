440dcc4ca559845ccae784d308d6310bccf2f23c
Brandon Williams <brandon@opetopic.com>
2012-01-16 18:10:54 -0500

    Small optimization to OPGradientView by forcing it's layer to rasterize.

--- OPUIKit/Source/OPGradientView.m
+++ OPUIKit/Source/OPGradientView.m
@@ -19,4 +19,25 @@
     return (CAGradientLayer*)self.layer;
 }
 
+-(id) init {
+    if (! (self = [super init]))
+        return nil;
+    self.layer.shouldRasterize = YES;
+    return self;
+}
+
+-(id) initWithFrame:(CGRect)frame {
+    if (! (self = [super initWithFrame:frame]))
+        return nil;
+    self.layer.shouldRasterize = YES;
+    return self;
+}
+
+-(id) initWithCoder:(NSCoder *)aDecoder {
+    if (! (self = [super initWithCoder:aDecoder]))
+        return nil;
+    self.layer.shouldRasterize = YES;
+    return self;
+}
+
 @end
