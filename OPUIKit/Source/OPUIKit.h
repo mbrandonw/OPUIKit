395560dd952725b2abc78134358d73a921e7bd3f
Brandon Williams <brandon@kickstarter.com>
2012-01-18 11:59:53 -0500

    Added a header for including everything.

---
+++ OPUIKit/Source/OPUIKit.h
@@ -0,0 +1,28 @@
+//
+//  OPUIKit.h
+//  OPUIKit
+//
+//  Created by Brandon Williams on 1/18/12.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "OPStyleProtocol.h"
+#import "OPStyle.h"
+#import "UIViewController+OPUIKit.h"
+#import "OPTabBar.h"
+#import "OPTabBarController.h"
+#import "OPTabBarItem.h"
+#import "OPSidebarNavigationController.h"
+#import "OPTableViewController.h"
+#import "OPViewController.h"
+#import "OPBarButtonItem.h"
+#import "OPGradient.h"
+#import "OPGradientView.h"
+#import "OPNavigationBar.h"
+#import "OPTextView.h"
+#import "OPView.h"
+#import "OPControl.h"
+#import "OPButton.h"
+#import "OPUIKitBlockDefinitions.h"
