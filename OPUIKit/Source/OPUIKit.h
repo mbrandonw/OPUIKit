7b150c3566810e8acfd83d0054f0ebb18ebaa9ad
Brandon Williams <brandon@kickstarter.com>
2012-01-19 15:59:23 -0500

    Put OPTabBarItemBadge in global header.

--- OPUIKit/Source/OPUIKit.h
+++ OPUIKit/Source/OPUIKit.h
@@ -14,6 +14,7 @@
 #import "OPTabBar.h"
 #import "OPTabBarController.h"
 #import "OPTabBarItem.h"
+#import "OPTabBarItemBadge.h"
 #import "OPSidebarNavigationController.h"
 #import "OPTableViewController.h"
 #import "OPViewController.h"
