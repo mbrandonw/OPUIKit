4a3f7c4622c81ec13ac581e0ec420a5cf065ae83
Brandon Williams <brandon@kickstarter.com>
2012-06-23 22:49:01 -0400

    In case # of tabs doesn't event divide tab bar width we force the last one to fit.

--- OPUIKit/Source/OPTabBar.m
+++ OPUIKit/Source/OPTabBar.m
@@ -271,6 +271,9 @@
     {
         CGFloat itemWidth = roundf(self.width / [self.items count]);
         item.frame = CGRectMake(roundf(itemWidth * (0.5f + index) - itemWidth/2.0f), item.top, itemWidth, item.height);
+        
+        if (index == [self.items count]-1)
+            item.width = self.width - (itemWidth * ([self.items count]-1));
     }
     else if (self.itemLayout == OPTabBarItemLayoutCenterGrouped)
     {
