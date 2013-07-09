0b488c4adbd80f94870c04d46c8a6e2e9231091d
Brandon Williams <brandon@kickstarter.com>
2013-07-09 16:51:55 -0400

    OPScrollViewController can handle keyboard events now.

--- OPUIKit/Source/OPScrollViewController.m
+++ OPUIKit/Source/OPScrollViewController.m
@@ -10,13 +10,38 @@
 
 @implementation OPScrollViewController
 
--(void) viewDidLoad {
-    self.view = [[UIScrollView alloc] initWithFrame:self.view.frame];
-    [super viewDidLoad];
+-(id) init {
+  if (! (self = [super init])) {
+    return nil;
+  }
+
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateInsetsForKeyboard:) name:UIKeyboardWillShowNotification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateInsetsForKeyboard:) name:UIKeyboardWillHideNotification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateInsetsForKeyboard:) name:UIKeyboardWillChangeFrameNotification object:nil];
+
+  return self;
+}
+
+-(void) dealloc {
+  [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+  [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+  [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillChangeFrameNotification object:nil];
+}
+
+-(void) loadView {
+  [super loadView];
+  self.view = [[UIScrollView alloc] initWithFrame:self.view.frame];
 }
 
 -(UIScrollView*) scrollView {
-    return [self.view isKindOfClass:[UIScrollView class]] ? (UIScrollView*)self.view : nil;
+  return [self.view isKindOfClass:[UIScrollView class]] ? (UIScrollView*)self.view : nil;
+}
+
+-(void) updateInsetsForKeyboard:(NSNotification*)notification {
+
+  CGRect keyboardRect = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+  [self.scrollView setContentInsetBottom:keyboardRect.size.height];
+  [self.scrollView setScrollIndicatorInsetBottom:keyboardRect.size.height];
 }
 
 @end
