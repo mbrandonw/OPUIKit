a336dba4211bebcae83b6f664288146366bc07cc
Brandon Williams <brandon@kickstarter.com>
2013-08-05 16:19:59 -0400

    Remove drawRect from OPNavigationBar for iOS 7 compatibility.

--- OPUIKit/Source/OPNavigationBar.m
+++ OPUIKit/Source/OPNavigationBar.m
@@ -47,22 +47,23 @@
 -(id) initWithCoder:(NSCoder *)aDecoder {
 	if (! (self = [super initWithCoder:aDecoder]))
 		return nil;
-    
-    self.clipsToBounds = NO;
-	
-    self.shadowView = [[OPGradientView alloc] initWithFrame:CGRectMake(0.0f, self.height, self.width, self.shadowHeight)];
-    self.shadowView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    self.shadowView.gradientLayer.startPoint = CGPointMake(0.5f, 1.0f);
-    self.shadowView.gradientLayer.endPoint = CGPointMake(0.5f, 0.0);
-    [self addSubview:self.shadowView];
-    
-    // apply stylings
-    [[[self class] styling] applyTo:self];
-    
-    // pass the background color to the tint color so that default bar button items inherit some of the styling
-    if (self.backgroundColor)
-        self.tintColor = self.backgroundColor;
-    
+
+  self.clipsToBounds = NO;
+
+  self.shadowView = [[OPGradientView alloc] initWithFrame:CGRectMake(0.0f, self.height, self.width, self.shadowHeight)];
+  self.shadowView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+  self.shadowView.gradientLayer.startPoint = CGPointMake(0.5f, 1.0f);
+  self.shadowView.gradientLayer.endPoint = CGPointMake(0.5f, 0.0);
+  [self addSubview:self.shadowView];
+
+  // pass the background color to the tint color so that default bar button items inherit some of the styling
+  if (self.backgroundColor) {
+    self.tintColor = self.backgroundColor;
+  }
+
+  // apply stylings
+  [[[self class] styling] applyTo:self];
+
 	return self;
 }
 
@@ -70,65 +71,20 @@
 #pragma mark Drawing methods
 #pragma mark -
 
--(void) drawRect:(CGRect)rect {
-    BOOL shouldCallSuper = YES;
-    CGContextRef c = UIGraphicsGetCurrentContext();
-	
-    // draw background images & colors
-    if (self.backgroundImage)
-    {
-        shouldCallSuper = NO;
-        
-        // figure out if we need to draw a stretchable image or a pattern image
-        if ([self.backgroundImage isStretchableImage])
-            [self.backgroundImage drawInRect:rect];
-        else
-            [self.backgroundImage drawAsPatternInRect:rect];
-    }
-    if (self.backgroundColor)
-    {
-        shouldCallSuper = NO;
-        
-        if (self.gradientAmount > 0.0f) {
-            [[OPGradient gradientWithColors:@[[self.backgroundColor lighten:self.gradientAmount], 
-                                             self.backgroundColor, 
-                                             [self.backgroundColor darken:self.gradientAmount]]] fillRectLinearly:rect];
-        }
-    }
-    
-    // apply gloss over everything
-    if (self.glossAmount)
-    {
-        shouldCallSuper = NO;
-        [$WAf(1.0f,self.glossAmount) set];
-        CGContextFillRect(c, CGRectMake(0.0f, 0.0f, rect.size.width, roundf(rect.size.height/2.0f + self.glossOffset)));
-    }
-    
-    // apply additional block based drawing
-    if (self.navigationBarDrawingBlock)
-    {
-        shouldCallSuper = NO;
-        self.navigationBarDrawingBlock(self, rect, c);
-    }
-    
-    if (shouldCallSuper)
-        [super drawRect:rect];
-}
-
 -(void) layoutSubviews {
-    [super layoutSubviews];
-    self.shadowView.frame = CGRectMake(0.0f, self.height, self.width, self.shadowView.height);
-    
-    if ([self.topItem.rightBarButtonItem isKindOfClass:[OPBarButtonItem class]] && [(OPBarButtonItem*)self.topItem.rightBarButtonItem isFlush])
-    {
-        self.topItem.rightBarButtonItem.customView.right = self.width;
-        self.topItem.rightBarButtonItem.customView.height = self.height;
-    }
-    if ([self.topItem.leftBarButtonItem isKindOfClass:[OPBarButtonItem class]] && [(OPBarButtonItem*)self.topItem.leftBarButtonItem isFlush])
-    {
-        self.topItem.leftBarButtonItem.customView.left = 0.0f;
-        self.topItem.leftBarButtonItem.customView.height = self.height;
-    }
+  [super layoutSubviews];
+  self.shadowView.frame = CGRectMake(0.0f, self.height, self.width, self.shadowView.height);
+
+  if ([self.topItem.rightBarButtonItem isKindOfClass:[OPBarButtonItem class]] && [(OPBarButtonItem*)self.topItem.rightBarButtonItem isFlush])
+  {
+    self.topItem.rightBarButtonItem.customView.right = self.width;
+    self.topItem.rightBarButtonItem.customView.height = self.height;
+  }
+  if ([self.topItem.leftBarButtonItem isKindOfClass:[OPBarButtonItem class]] && [(OPBarButtonItem*)self.topItem.leftBarButtonItem isFlush])
+  {
+    self.topItem.leftBarButtonItem.customView.left = 0.0f;
+    self.topItem.leftBarButtonItem.customView.height = self.height;
+  }
 }
 
 #pragma mark -
@@ -136,30 +92,30 @@
 #pragma mark -
 
 -(void) setShadowHeight:(CGFloat)shadowHeight {
-    _shadowHeight = shadowHeight;
-    self.shadowView.height = shadowHeight;
-    [self setShadowHidden:(shadowHeight == 0.0f)];
-    [self setNeedsLayout];
+  _shadowHeight = shadowHeight;
+  self.shadowView.height = shadowHeight;
+  [self setShadowHidden:(shadowHeight == 0.0f)];
+  [self setNeedsLayout];
 }
 
 -(void) setShadowColors:(NSArray *)shadowColors {
-    _shadowColors = shadowColors;
-    self.shadowView.gradientLayer.colors = shadowColors;
-    [self setNeedsLayout];
+  _shadowColors = shadowColors;
+  self.shadowView.gradientLayer.colors = shadowColors;
+  [self setNeedsLayout];
 }
 
 -(void) setShadowHidden:(BOOL)hidden {
-    [self setShadowHidden:hidden animated:NO];
+  [self setShadowHidden:hidden animated:NO];
 }
 
 -(void) setShadowHidden:(BOOL)hidden animated:(BOOL)animated {
-    _shadowHeight = hidden;
-    
-    [UIView animateWithDuration:(0.3f * animated) animations:^{
-        self.shadowView.alpha = hidden ? 0.0f : 1.0f;
-    } completion:^(BOOL finished) {
-        self.shadowView.hidden = hidden;
-    }];
+  _shadowHeight = hidden;
+
+  [UIView animateWithDuration:(0.3f * animated) animations:^{
+    self.shadowView.alpha = hidden ? 0.0f : 1.0f;
+  } completion:^(BOOL finished) {
+    self.shadowView.hidden = hidden;
+  }];
 }
 
 #pragma mark -
@@ -167,64 +123,64 @@
 #pragma mark -
 
 -(void) showStatusView:(UIView*)statusView {
-    [self showStatusView:statusView hideAfter:4.0f];
+  [self showStatusView:statusView hideAfter:4.0f];
 }
 
 -(void) showStatusView:(UIView*)statusView hideAfter:(NSTimeInterval)hideAfter {
-    [self showStatusView:statusView hideAfter:hideAfter completion:nil];
+  [self showStatusView:statusView hideAfter:hideAfter completion:nil];
 }
 
 -(void) showStatusView:(UIView*)statusView hideAfter:(NSTimeInterval)hideAfter completion:(void(^)(void))completion {
-    
-    if (statusView == self.visibleStatusView)
-    {
-        // do nothing
-    }
-    else
-    {
-        [self.visibleStatusView removeFromSuperview];
-        self.visibleStatusView = statusView;
-        statusView.width = self.width;
-        
-        self.statusViewContainer = self.statusViewContainer ?: [UIView new];
-        [self addSubview:self.statusViewContainer];
-        self.statusViewContainer.top = self.height;
-        self.statusViewContainer.width = self.width;
-        self.statusViewContainer.height = self.visibleStatusView.height;
-        self.statusViewContainer.clipsToBounds = YES;
-        
-        statusView.left = 0.0f;
-        statusView.bottom = 0.0f;
-        [self.statusViewContainer addSubview:statusView];
-        
-        [UIView animateWithDuration:0.3f animations:^{
-            statusView.top = 0.0f;
-        } completion:^(BOOL finished) {
-            [self performSelector:@selector(hideStatusView) withObject:nil afterDelay:hideAfter];
-            if (completion)
-                completion();
-        }];
-    }
+
+  if (statusView == self.visibleStatusView)
+  {
+    // do nothing
+  }
+  else
+  {
+    [self.visibleStatusView removeFromSuperview];
+    self.visibleStatusView = statusView;
+    statusView.width = self.width;
+
+    self.statusViewContainer = self.statusViewContainer ?: [UIView new];
+    [self addSubview:self.statusViewContainer];
+    self.statusViewContainer.top = self.height;
+    self.statusViewContainer.width = self.width;
+    self.statusViewContainer.height = self.visibleStatusView.height;
+    self.statusViewContainer.clipsToBounds = YES;
+
+    statusView.left = 0.0f;
+    statusView.bottom = 0.0f;
+    [self.statusViewContainer addSubview:statusView];
+
+    [UIView animateWithDuration:0.3f animations:^{
+      statusView.top = 0.0f;
+    } completion:^(BOOL finished) {
+      [self performSelector:@selector(hideStatusView) withObject:nil afterDelay:hideAfter];
+      if (completion)
+        completion();
+    }];
+  }
 }
 
 -(void) hideStatusView {
-    [self hideStatusView:nil];
+  [self hideStatusView:nil];
 }
 
 -(void) hideStatusView:(void(^)(void))completion {
-    
-    [NSObject cancelPreviousPerformRequestsWithTarget:self];
-    if (self.visibleStatusView)
-    {
-        [UIView animateWithDuration:0.3f delay:0.0f options:UIViewAnimationOptionBeginFromCurrentState animations:^{
-            self.visibleStatusView.bottom = 0.0f;
-        } completion:^(BOOL finished) {
-            [self.visibleStatusView removeFromSuperview];
-            self.visibleStatusView = nil;
-            if (completion)
-                completion();
-        }];
-    }
+
+  [NSObject cancelPreviousPerformRequestsWithTarget:self];
+  if (self.visibleStatusView)
+  {
+    [UIView animateWithDuration:0.3f delay:0.0f options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+      self.visibleStatusView.bottom = 0.0f;
+    } completion:^(BOOL finished) {
+      [self.visibleStatusView removeFromSuperview];
+      self.visibleStatusView = nil;
+      if (completion)
+        completion();
+    }];
+  }
 }
 
 @end
@@ -232,42 +188,42 @@
 @implementation UIViewController (OPNavigationBar)
 
 -(void) showStatusView:(UIView*)statusView {
-    [self showStatusView:statusView hideAfter:4.0f];
+  [self showStatusView:statusView hideAfter:4.0f];
 }
 
 -(void) showStatusView:(UIView*)statusView hideAfter:(NSTimeInterval)hideAfter {
-    [self showStatusView:statusView hideAfter:hideAfter completion:nil];
+  [self showStatusView:statusView hideAfter:hideAfter completion:nil];
 }
 
 -(void) showStatusView:(UIView*)statusView hideAfter:(NSTimeInterval)hideAfter completion:(void(^)(void))completion {
-    
-    if ([self isViewVisible])
-    {
-        OPNavigationBar *navigationBar = (OPNavigationBar*)self.navigationController.navigationBar;
-        if (! navigationBar && [self respondsToSelector:@selector(navigationBar)]) {
-            navigationBar = (OPNavigationBar*)[(id)self navigationBar];
-        }
-        if ([navigationBar isKindOfClass:[OPNavigationBar class]])
-        {
-            [navigationBar showStatusView:statusView hideAfter:hideAfter completion:completion];
-        }
-    }
-}
 
--(void) hideStatusView {
-    [self hideStatusView:nil];
-}
-
--(void) hideStatusView:(void(^)(void))completion {
-    
+  if ([self isViewVisible])
+  {
     OPNavigationBar *navigationBar = (OPNavigationBar*)self.navigationController.navigationBar;
     if (! navigationBar && [self respondsToSelector:@selector(navigationBar)]) {
-        navigationBar = (OPNavigationBar*)[(id)self navigationBar];
+      navigationBar = (OPNavigationBar*)[(id)self navigationBar];
     }
     if ([navigationBar isKindOfClass:[OPNavigationBar class]])
     {
-        [navigationBar hideStatusView:completion];
+      [navigationBar showStatusView:statusView hideAfter:hideAfter completion:completion];
     }
+  }
+}
+
+-(void) hideStatusView {
+  [self hideStatusView:nil];
+}
+
+-(void) hideStatusView:(void(^)(void))completion {
+
+  OPNavigationBar *navigationBar = (OPNavigationBar*)self.navigationController.navigationBar;
+  if (! navigationBar && [self respondsToSelector:@selector(navigationBar)]) {
+    navigationBar = (OPNavigationBar*)[(id)self navigationBar];
+  }
+  if ([navigationBar isKindOfClass:[OPNavigationBar class]])
+  {
+    [navigationBar hideStatusView:completion];
+  }
 }
 
 @end
