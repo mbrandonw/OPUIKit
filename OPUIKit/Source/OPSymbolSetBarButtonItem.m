c35c4f66943c71e477f74d169278d4f36bbafac7
Brandon Williams <brandon@kickstarter.com>
2013-09-05 11:42:56 -0400

    Non hacky way of handling bar button item insets.

--- OPUIKit/Source/OPSymbolSetBarButtonItem.m
+++ OPUIKit/Source/OPSymbolSetBarButtonItem.m
@@ -11,12 +11,17 @@
 @implementation OPSymbolSetBarButtonItem
 
 -(instancetype) initWithGlyph:(NSString*)glyph side:(OPBarButtonItemSide)side target:(id)target action:(SEL)selector {
-  NSString *title = [NSString stringWithFormat:side == OPBarButtonItemSideLeft ? @"  %@" : @"%@  ", glyph];
-  if (! (self = [self initWithTitle:title style:UIBarButtonItemStylePlain target:target action:selector])) {
+  if (! (self = [self initWithTitle:glyph style:UIBarButtonItemStyleBordered target:target action:selector])) {
     return nil;
   }
 
   [self setTitleTextAttributes:@{UITextAttributeFont: [UIFont ssStandardFontWithSize:16.0f]} forState:UIControlStateNormal];
+
+  if (side == OPBarButtonItemSideLeft) {
+    [self setTitlePositionAdjustment:UIOffsetMake(8.0f, 2.0f) forBarMetrics:UIBarMetricsDefault];
+  } else {
+    [self setTitlePositionAdjustment:UIOffsetMake(-8.0f, 2.0f) forBarMetrics:UIBarMetricsDefault];
+  }
 
   return self;
 }
