6ab1c9b7d0648b0882e6834e97a38e0e1b00317e
mbrandonw <mbw234@gmail.com>
2014-04-24 11:16:33 -0400

    Use 2 space tabs.

--- OPUIKit/Source/OPNavigationController.m
+++ OPUIKit/Source/OPNavigationController.m
@@ -33,36 +33,36 @@
 #pragma mark -
 
 +(id) controller {
-    return [self controllerWithRootViewController:nil];
+  return [self controllerWithRootViewController:nil];
 }
 
 +(id) controllerWithRootViewController:(UIViewController*)rootViewController {
-	
+
 	OPNavigationController *controller = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil] lastObject];
-    
-    if (rootViewController)
-        controller.viewControllers = @[rootViewController];
+
+  if (rootViewController)
+    controller.viewControllers = @[rootViewController];
 	controller.delegate = controller;
-    
+
 	return controller;
 }
 
 -(id) initWithCoder:(NSCoder *)aDecoder {
-    if (! (self = [super initWithCoder:aDecoder]))
-        return nil;
-    
-    // apply stylings
-    [[[self class] styling] applyTo:self];
-    
-    return self;
+  if (! (self = [super initWithCoder:aDecoder]))
+    return nil;
+
+  // apply stylings
+  [[[self class] styling] applyTo:self];
+
+  return self;
 }
 
 - (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    DLogClassAndMethod();
-    
-    // Release any cached data, images, etc. that aren't in use.
+  // Releases the view if it doesn't have a superview.
+  [super didReceiveMemoryWarning];
+  DLogClassAndMethod();
+
+  // Release any cached data, images, etc. that aren't in use.
 }
 
 #pragma mark -
@@ -70,9 +70,9 @@
 #pragma mark -
 
 -(void) viewDidLoad {
-    [super viewDidLoad];
-    DLogClassAndMethod();
-    [self configurePopRecognizer];
+  [super viewDidLoad];
+  DLogClassAndMethod();
+  [self configurePopRecognizer];
 }
 
 #pragma mark -
@@ -80,41 +80,41 @@
 #pragma mark -
 
 -(void) setAllowSwipeToPop:(NSInteger)a {
-    _allowSwipeToPop = a;
-    
-    // handle gesture recognizers only if our view is loaded (to prevent accidental loading of the view)
-    if ([self isViewLoaded])
-        [self configurePopRecognizer];
+  _allowSwipeToPop = a;
+
+  // handle gesture recognizers only if our view is loaded (to prevent accidental loading of the view)
+  if ([self isViewLoaded])
+    [self configurePopRecognizer];
 }
 
 -(void) configurePopRecognizer {
-    
-    if (self.allowSwipeToPop)
-    {
-        if (! [self.view.gestureRecognizers containsObject:self.popRecognizer])
-            [self.view addGestureRecognizer:self.popRecognizer];
-    }
-    else
-    {
-        [self.view removeGestureRecognizer:self.popRecognizer];
-        self.popRecognizer = nil;
-    }
+
+  if (self.allowSwipeToPop)
+  {
+    if (! [self.view.gestureRecognizers containsObject:self.popRecognizer])
+      [self.view addGestureRecognizer:self.popRecognizer];
+  }
+  else
+  {
+    [self.view removeGestureRecognizer:self.popRecognizer];
+    self.popRecognizer = nil;
+  }
 }
 
 -(UISwipeGestureRecognizer*) popRecognizer {
-    if (! _popRecognizer && self.allowSwipeToPop)
-    {
-        self.popRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swiped:)];
-        _popRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
-        _popRecognizer.numberOfTouchesRequired = 1;
-    }
-    return _popRecognizer;
+  if (! _popRecognizer && self.allowSwipeToPop)
+  {
+    self.popRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swiped:)];
+    _popRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+    _popRecognizer.numberOfTouchesRequired = 1;
+  }
+  return _popRecognizer;
 }
 
 -(void) swiped:(UISwipeGestureRecognizer*)sender {
-    
-    if (sender.state == UIGestureRecognizerStateEnded)
-        [self popViewControllerAnimated:YES];
+
+  if (sender.state == UIGestureRecognizerStateEnded)
+    [self popViewControllerAnimated:YES];
 }
 
 #pragma mark -
@@ -122,18 +122,18 @@
 #pragma mark -
 
 -(UIViewController*) popViewControllerAnimated:(BOOL)animated {
-    
-    UIViewController *retVal = [super popViewControllerAnimated:animated];
-    
-    if ([retVal respondsToSelector:@selector(navigationController:isPoppingSelf:)])
-        [(id)retVal navigationController:self isPoppingSelf:animated];
-    
-    return retVal;
+
+  UIViewController *retVal = [super popViewControllerAnimated:animated];
+
+  if ([retVal respondsToSelector:@selector(navigationController:isPoppingSelf:)])
+    [(id)retVal navigationController:self isPoppingSelf:animated];
+
+  return retVal;
 }
 
 -(void) viewDidLayoutSubviews {
-    [super viewDidLayoutSubviews];
-    [self layoutToolbarView];
+  [super viewDidLayoutSubviews];
+  [self layoutToolbarView];
 }
 
 #pragma mark -
@@ -141,18 +141,18 @@
 #pragma mark -
 
 - (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    
-    if([navigationController.viewControllers count] > 1 && 
-       ! viewController.navigationItem.hidesBackButton && 
-       ! viewController.navigationItem.leftBarButtonItem)
+
+  if([navigationController.viewControllers count] > 1 &&
+     ! viewController.navigationItem.hidesBackButton &&
+     ! viewController.navigationItem.leftBarButtonItem)
 	{
-        UIViewController *lastController = [navigationController.viewControllers objectAtIndex:[navigationController.viewControllers indexOfObject:viewController]-1];
-        
-        viewController.navigationItem.leftBarButtonItem = [OPBackBarButtonItem buttonWithTitle:
-                                                           lastController.title ?: NSLocalizedString(@"Back", @"UIBarButtonItem default back label")
-                                                                                        target:self 
-                                                                                        action:@selector(popViewControllerWithAnimation)];
-    }
+    UIViewController *lastController = [navigationController.viewControllers objectAtIndex:[navigationController.viewControllers indexOfObject:viewController]-1];
+
+    viewController.navigationItem.leftBarButtonItem = [OPBackBarButtonItem buttonWithTitle:
+                                                       lastController.title ?: NSLocalizedString(@"Back", @"UIBarButtonItem default back label")
+                                                                                    target:self
+                                                                                    action:@selector(popViewControllerWithAnimation)];
+  }
 }
 
 -(void) navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
